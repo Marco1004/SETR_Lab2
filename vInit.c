450002032cb14ae7b4f4fa2cc1f4e458e031176e
Marco1004 <60965281+Marco1004@users.noreply.github.com>
2022-03-15 18:15:30 +0000

    vInit initial commit

---
+++ vInit.c
@@ -0,0 +1,12 @@
+int* vInit(int size){
+    /*
+    int [size] numbers;
+    for (int i= 0; i<size; i++){
+        numbers[i]= rand();
+    }
+    return numbers;
+    */
+   int n;
+   n= rand();
+   return n
+}

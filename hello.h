089c489c99562da56354be249b3c34efcb197647
Daniel Almeida <danix@pop-os.localdomain>
2022-03-15 18:38:35 +0000

    hello updated to use doxygen

--- hello.h
+++ hello.h
@@ -1,1 +1,17 @@
+/** @file hello.h
+ * @brief Brief description of hello 
+ *
+ * Just prints "Hello"
+ * 
+ * 
+ * @author Daniel Almeida
+ * @date 15 March 2022
+ * @bug If works there's no bugs.
+ */
+
+#ifndef _module1_h
+#define _module1_h
+
 void hello();
+
+#endif
